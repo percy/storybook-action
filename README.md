ae34c47aaaede08e6a1a985954a4cecf9cab21b4
Robert DeLuca <robertdeluca19@gmail.com>
2019-11-15 16:59:59 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Percy Storybook GitHub Action
+
+A GitHub action to run Percy Storybook.
+
+## Quick start
+
+To use the Percy exec GitHub action you will need to add a new step to your
+actions config using the `percy/storybook-action` action. Using the default
+settings, you can just use the action directly. You will also need to set your
+`PERCY_TOKEN` in your GitHub projects settings.
+
+This is a sample config using the default setup:
+
+``` yaml
+name: CI
+on: [push, pull_request]
+jobs:
+  default:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@master
+      - name: Install
+        run: yarn
+      - name: Percy Test
+        uses: percy/storybook-action@master
+        env:
+          PERCY_TOKEN: ${{ secrets.PERCY_TOKEN }}
+```
+
+And this is a setup that passes a custom command, which allows for any
+configuraion:
+
+``` yaml
+name: CI
+on: [push, pull_request]
+jobs:
+  custom_command:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout
+        uses: actions/checkout@master
+      - name: Install
+        run: yarn
+      - name: Percy Test
+        uses: percy/storybook-action@master
+        with:
+          custom-command: 'yarn storybook:percy'
+        env:
+          PERCY_TOKEN: ${{ secrets.PERCY_TOKEN }}
+```
---
+++ src/index.js
@@ -0,0 +1,51 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+const io = require('@actions/io');
+const pkg = require('../package.json');
+
+const ACTION_UA = `${pkg.name}/${pkg.version}`;
+
+// Sets the required env info for Percy to work correctly
+function setPercyBranchBuildInfo(pullRequestNumber) {
+  if (!!pullRequestNumber) {
+    let prBranch = github.context.payload.pull_request.head.ref;
+
+    core.exportVariable('PERCY_BRANCH', prBranch);
+    core.exportVariable('PERCY_PULL_REQUEST', pullRequestNumber);
+  } else {
+    core.exportVariable('PERCY_BRANCH', github.context.payload.ref.replace('refs/heads/', ''));
+  }
+}
+
+(async () => {
+  try {
+    let customCommand = core.getInput('custom-command');
+    let pullRequestNumber = github.context.payload.number;
+
+    // Set the CI builds user agent
+    core.exportVariable('PERCY_GITHUB_ACTION', ACTION_UA);
+
+    // Set the PR # (if available) and branch name
+    setPercyBranchBuildInfo(pullRequestNumber);
+
+    if (customCommand) {
+      // Run the passed command
+      await exec.exec(`${customCommand}`);
+
+      return;
+    } else {
+      // happy path
+      let npxPath = await io.which('npx', true);
+
+      // Build the storybook project
+      await exec.exec(`"${npxPath}" build-storybook`);
+      // Run Percy over the build output
+      await exec.exec(`"${npxPath}" percy-storybook`);
+
+      return;
+    }
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})();

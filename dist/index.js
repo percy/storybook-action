cd65e6b5d41fa8b0297daaf6070d36ffa3a14eb7
Robert DeLuca <robertdeluca19@gmail.com>
2020-07-30 17:19:28 -0500

    v0.1.5

--- dist/index.js
+++ dist/index.js
@@ -9403,7 +9403,7 @@ module.exports = {"activity":{"checkStarringRepo":{"method":"GET","params":{"own
 /***/ 731:
 /***/ (function(module) {
 
-module.exports = {"devDependencies":{"@zeit/ncc":"^0.20.5"},"dependencies":{"@actions/core":"^1.2.0","@actions/exec":"^1.0.3","@actions/github":"^1.1.0","@actions/io":"^1.0.2"},"scripts":{"precommit":"yarn build && git add dist/index.js","build":"ncc build src/index.js"},"license":"MIT","author":"Perceptual Inc.","keywords":["GitHub action","Percy","Storybook","visual testing"],"repository":"https://github.com/percy/storybook-action","main":"dist/index.js","description":"A GitHub Action for running Storybook visual tests with Percy","version":"0.1.4","name":"storybook"};
+module.exports = {"devDependencies":{"@zeit/ncc":"^0.20.5"},"dependencies":{"@actions/core":"^1.2.0","@actions/exec":"^1.0.3","@actions/github":"^1.1.0","@actions/io":"^1.0.2"},"scripts":{"precommit":"yarn build && git add dist/index.js","build":"ncc build src/index.js"},"license":"MIT","author":"Perceptual Inc.","keywords":["GitHub action","Percy","Storybook","visual testing"],"repository":"https://github.com/percy/storybook-action","main":"dist/index.js","description":"A GitHub Action for running Storybook visual tests with Percy","version":"0.1.5","name":"storybook"};
 
 /***/ }),
 
